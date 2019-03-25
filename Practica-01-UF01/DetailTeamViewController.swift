f7720b6843845f15b7293692464dcc61473ad5fd
Alumne <Alumne@iMac-de-manteniment.local>
2019-03-25 17:46:00 +0100

    First version of storyboard

---
+++ Practica-01-UF01/DetailTeamViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailTeamViewController.swift
+//  Practica-01-UF01
+//
+//  Created by Alumne on 25/03/2019.
+//  Copyright © 2019 Alumne. All rights reserved.
+//
+
+import UIKit
+
+class DetailTeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Practica-01-UF01/ListOfTeams.swift
@@ -0,0 +1,9 @@
+//
+//  ListOfTeams.swift
+//  Practica-01-UF01
+//
+//  Created by Alumne on 25/03/2019.
+//  Copyright © 2019 Alumne. All rights reserved.
+//
+
+import Foundation
