f4536afb42983268da6c4a2334f4b3cd4b959eb3
Pol Piedra <polpica@hotmail.com>
2019-04-05 11:16:16 +0200

    Team names well displayed in Picker

--- Practica-01-UF01/EditMatchViewController.swift
+++ Practica-01-UF01/EditMatchViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class EditMatchViewController: UIViewController {
-
+class EditMatchViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     var database : FMDatabase = SQLiteSingleton.getInstance()
     let teamManager : TeamManager = (SQLFactory.createFactory(2) as! TeamManager)
     var pickerData: [String] = [String]()
@@ -19,6 +19,12 @@ class EditMatchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.pickerLocal.delegate = self
+        self.pickerLocal.dataSource = self
+        self.pickerAway.delegate = self
+        self.pickerAway.dataSource = self
+        
         if database.open() {
         let teams = teamManager.readRecords(database)
             if teams.isEmpty{
@@ -37,6 +43,25 @@ class EditMatchViewController: UIViewController {
         }
     }
     
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+
 
     /*
     // MARK: - Navigation
